e73914d285d9c69851326f7c76887512bbc7c42d
Vincent Sourice <v.sourice@purjus.fr>
2019-04-30 07:29:41 +0200

    theme bsaap git

---
+++ user/themes/bsaap/README.md
@@ -0,0 +1,7 @@
+# Bsaap Theme
+
+The **Bsaap** Theme is for [Grav CMS](http://github.com/getgrav/grav).  This README.md file should be modified to describe the features, installation, configuration, and general usage of this theme.
+
+## Description
+
+Theme for officines
---
+++ user/themes/bsaap/src/js/app.js
@@ -0,0 +1,10 @@
+import UIkit from 'uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
+
+require('./../scss/app.scss')
+
+// loads the Icon plugin
+UIkit.use(Icons)
+
+// components can be called from the imported UIkit reference
+// UIkit.notification('Hello world.')
